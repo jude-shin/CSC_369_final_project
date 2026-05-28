59d28c07c7499d599395432ef1fb017a06936d5d
JimmyChin946 <Judes946@gmail.com>
2026-05-28 13:55:29 -0700

    main similarity

---
+++ main/src/main/scala/example/App.scala
@@ -0,0 +1,22 @@
+package example
+
+import org.apache.spark.SparkContext._
+import scala.io._
+import org.apache.spark.{ SparkConf, SparkContext }
+import org.apache.spark.rdd._
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+import scala.collection._
+
+object App {
+  def main(args: Array[String]) {
+    // Don't log a bunch of the junk
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+
+    // Spark setup
+    val conf = new SparkConf().setAppName("App")
+    val sc = new SparkContext(conf)
+
+  }
+}
---
+++ main/src/main/scala/example/Cosine.scala
@@ -0,0 +1,14 @@
+package example
+
+import org.apache.spark.SparkContext._
+import scala.io._
+import org.apache.spark.{ SparkConf, SparkContext }
+import org.apache.spark.rdd._
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+import scala.collection._
+
+import scala.util.parsing.json.JSON
+
+object Cosine {
+}
