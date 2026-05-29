e7b4f0b91dbcb2ab6821cb4b2b1b461a0bd087e6
JimmyChin946 <Judes946@gmail.com>
2026-05-29 00:47:01 -0700

    moved everything to Parser.scala

--- jude/src/main/scala/example/App.scala
+++ jude/src/main/scala/example/App.scala
@@ -19,45 +19,9 @@ object App {
     val sc = new SparkContext(conf)
 
     // Only have to do once
-    preprocessData(sc, 
+    Parser.preprocessData(sc, 
       "input/final/reviews/", 
       "input/final/metadata/",
       "processed")
-  }
-
-  def preprocessData(sc: SparkContext, reviewsPath: String, metadatPath: String, processedPath: String) = {
-    /*******************/
-    /* Inital Datasets */
-    /*******************/
-    // Reviews Dataset
-    val reviewsRdd = sc.textFile(reviewsPath)
-      .map(Parser.parseReviews)
-      .filter(_ != null)
-      .map(r => (r._6, r))  // Review._6 is the parent_asin
-
-    // Metadata Dataset
-    val metadataRdd = sc.textFile(metadatPath)
-      .map(Parser.parseMetadata)
-      .filter(_ != null)
-      .map(m => (m._13, m)) // Metadata._13 is the parent_asin
-
-    /*********************/
-    /* JOIN the datasets */
-    /*********************/
-    // Join the reviews and the metadata on the parent_asin number
-    val joined = reviewsRdd.join(metadataRdd)
-
-    joined
-      .map ({
-        case (parent_asin, (r, m)) =>
-          val rating = r._1
-          val user_id = r._3 // assuming Review._3 is user_id
-          ((user_id, parent_asin), rating)
-      })
-
-    /****************/
-    /* Save to File */
-    /****************/
-    .saveAsTextFile(processedPath)
   }
 }
